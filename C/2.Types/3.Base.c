11371f30d06d09e4ca3295ed06ce845fc2fdef88
Theo Lefebvre <theo.lefebvre@ens.uvsq.fr>
2020-06-11 18:31:55 +0200

    Base possible

---
+++ C/2.Types/3.Base.c
@@ -0,0 +1,19 @@
+/*
+
+Il vous est possible de préciser la base d’une constante entière en utilisant des préfixes. 
+Ces préfixes sont 0 pour les constantes octales et 0x ou 0X pour les constantes hexadécimales.
+
+*/
+
+
+//Base 10
+int decimal = 10;
+
+//Base 8
+int octal = 08;
+
+//Base 16
+int hexadecimal = 0xFF;
+
+//Base 2 (Pas dans le standart du C, extension de GCC)
+int binaire = 0b1001;
